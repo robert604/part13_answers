16a596b486f6fbcf10c57a409cd20aa3f59e30cf
robert604 <robtemper@gmail.com>
2022-03-10 13:02:50 -0500

    submission for 13.7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,24 +2,37 @@ const router = require('express').Router()
 
 const { Blog } = require('../models')
 
+const errorHandler = (error,req,res,next) => {
+  console.error('got an error---------',error.message);
+  return res.status(400).send({error:`got error${error.message}`})
+}
+
 router.get('/', async (req, res) => {
   const blogs = await Blog.findAll()
   res.json(blogs)
 })
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res,next) => {
   try {
     const blog = await Blog.create(req.body)
     res.json(blog)
   } catch(error) {
-    return res.status(400).json({ error })
+    next(error)    
+    //return res.status(400).json({ error })
   }
 })
 
 const blogFinder = async (req, res, next) => {
-  req.blog = await Blog.findByPk(req.params.id)
-  next()
+  try {
+    req.blog = await Blog.findByPk(req.params.id)
+    next()
+  } catch(error) {
+    next(error)
+  }
 }
+
+
+
 
 router.get('/:id', blogFinder, async (req, res) => {
   if (req.blog) {
@@ -36,13 +49,15 @@ router.delete('/:id', blogFinder, async (req, res) => {
   res.status(204).end()
 })
 
-router.put('/:id', blogFinder, async (req, res) => {
+router.put('/:id', blogFinder,errorHandler, async (req, res,next) => {
   if (req.blog) {
     req.blog.likes = req.body.likes
     await req.blog.save()
     res.json(req.blog)
   } else {
-    res.status(404).end()
+    const error = new Error("Can't find blog for changing likes")
+    next(error)
+    //res.status(404).end()
   }
 })
 
